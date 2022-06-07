8c781445d2d73f280d3578519bd82b19909eb025
Sikako <sikako.setp08@nctu.edu.tw>
2022-06-07 09:01:44 +0800

    func that can generate random strings

---
+++ randomString.c
@@ -0,0 +1,18 @@
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(){
+	int i, LEN = 5 + 1;
+	srandom(time(NULL));
+	unsigned char text[LEN];
+	memset(text, 1, LEN);
+	for(i = 0; i< LEN - 1; i++){
+		text[i] = (unsigned char) (rand() % 255 + 1);
+	}
+	text[LEN-1] = 0;
+	printf("plain-text:");
+	printf("strlen(text)=%zd\n", strlen(text));
+}
