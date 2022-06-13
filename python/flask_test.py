5a2da3e927edbd178dd14bc135ce84218408198b
sikako <jack71805@gmail.com>
2022-06-13 07:01:28 -0700

    update flask_test.py be able to execute attack app

--- python/flask_test.py
+++ python/flask_test.py
@@ -6,10 +6,22 @@ from flask import Flask,request
 import os
 
 app = Flask(__name__)
-@app.route("/", methods=["GET"])
-def hello():
-    name = request.args.get('name')
-    return "Hello World!!"+name
+@app.route("/start", methods=["GET"])
+def start_pid():
+    cmd = "sudo ../tools/udp_attack 172.24.19.216 80"
+    r = os.system(cmd)
+    return str(r)
+
+@app.route("/stop")
+def close_pid():
+    app = "udp_attack"
+    pid = os.system(f"pidof {app}")
+    cmd = f"sudo kill -9 {pid}"
+    r = os.system(cmd)
+    return str(r)
+
+
+
 
 if __name__ == '__main__':
     app.run(debug=True,host="0.0.0.0",port="9999")
