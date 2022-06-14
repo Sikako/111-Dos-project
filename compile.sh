d1cb22393781656b5dbeb73b0618e39803e28085
sikako <sikako.setp08@nctu.edu.tw>
2022-06-14 11:08:33 +0800

    update

--- compile.sh
+++ compile.sh
@@ -2,7 +2,7 @@
 
     gcc -o tools/tcp_attack tcp_attack.c header/init_header.c header/CRC16_check.c -lpthread >&2
     gcc -o tools/udp_attack udp_attack.c header/init_header.c header/CRC16_check.c -lpthread >&2
-    gcc -o tools/smurf_attack smurf.cp.c  header/CRC16_check.c header/init_header.c header/pause.c -lpthread >&2
+    gcc -o tools/smurf_attack smurf.c  header/CRC16_check.c header/init_header.c header/pause.c -lpthread >&2
 if [ "$?" = "0" ]
 then
     echo "Compile Succeses at ./tools"   
